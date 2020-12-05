fbc65aa3fcc45125bce9dffe39d03573c7a42fd3
Dimitris <dimitrisbl@hotmail.com>
2020-12-05 20:03:23 +0200

    location service with FusedLocationProviderClient API

---
+++ app/src/main/java/com/example/agroproject/LocationService.java
@@ -0,0 +1,74 @@
+package com.example.agroproject;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+
+public class LocationService extends Service {
+
+    public static final int DEFAULT_UPDATE_INTERNAL = 30;
+
+    public static final int FAST_UPDATE_INTERNAL = 5;
+
+    // Google's API for location service
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    // Location callback
+    private LocationCallback locationCallback;
+
+    // Location request
+    private LocationRequest locationRequest;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize FusedLocationProviderClient object
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.i("latitude: ", " "+locationResult.getLastLocation().getLatitude());
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        requestLocation();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocation(){
+        // Initialize LocationRequest object
+        locationRequest = new LocationRequest();
+
+        // For high accuracy location
+        locationRequest.setInterval(1000);
+
+
+        //PRIORITY_HIGH_ACCURACY using from the gps
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+}
--- app/src/main/java/com/example/agroproject/MainActivity.java
+++ app/src/main/java/com/example/agroproject/MainActivity.java
@@ -1,14 +1,157 @@
 package com.example.agroproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ApplicationExitInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Location permission request code
+    private static final int LOCATION_PERMISSION_CODE = 1;
+
+    // FusedLocationProviderClient
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    // Location callback
+    private LocationCallback locationCallback;
+
+    // Location request
+    private LocationRequest locationRequest;
+
+    //-------------------------------------------
+
+
+    // current view
+    private View currentView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Current view
+        currentView = findViewById(android.R.id.content);
+
+        // Initialize FusedLocationProviderClient object
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Initialize a location request
+//        locationRequest = LocationRequest.create();
+        locationRequest = new LocationRequest();
+
+        //PRIORITY_HIGH_ACCURACY using from the gps
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Location callBack method
+        locationCallBackExecute();
+
+        if(Build.VERSION.SDK_INT >= 23){
+
+           if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            //permission question
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+
+
+           }else{
+               fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+               //Start the location service
+               //startLocationService();
+           }
+
+
+        }else{
+
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+
+            //Start the location service
+            //startLocationService();
+
+        }
+
     }
+
+    public void startLocationService(){
+        Intent locationServiceIntent = new Intent(this, LocationService.class);
+        startService(locationServiceIntent);
+    }
+
+
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == LOCATION_PERMISSION_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+
+            }else{
+                Snackbar.make(currentView, "Permission is not accepted", Snackbar.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    /*
+       This method handle the requestLocationUpdates.
+       Used for receiving notifications from the FusedLocationProviderApi
+       when the device location has changed or can no longer be determined.
+       */
+    private void locationCallBackExecute(){
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    //set coordinates
+                    Toast.makeText(MainActivity.this,"latitude from locationCallBackExecute: "
+                            +location.getLatitude(), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+    }
+
 }

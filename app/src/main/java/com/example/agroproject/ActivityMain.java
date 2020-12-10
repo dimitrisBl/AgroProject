aa11e0930303724905e8ad1351a2d1359cbe79a9
Dimitris <dimitrisbl@hotmail.com>
2020-12-10 22:54:21 +0200

    get device location from gps

--- app/src/main/java/com/example/agroproject/ActivityMain.java
+++ app/src/main/java/com/example/agroproject/ActivityMain.java
@@ -1,13 +1,21 @@
 package com.example.agroproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ActivityMain extends AppCompatActivity {
+
+    // Location permission request code
+    private static final int LOCATION_PERMISSION_CODE = 1;
 
     private Button button;
 
@@ -16,10 +24,39 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Location permission check
+       // permissionRequestForLocation();
+
         button = findViewById(R.id.map_btn);
         button.setOnClickListener(onButtonClickListener);
 
+
+
     }
+
+
+    private void permissionRequestForLocation() {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                //permission question
+                ActivityCompat.requestPermissions(this, new String[]
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+
+            }
+        }
+    }
+
 
 
 
--- app/src/main/java/com/example/agroproject/MapActivity.java
+++ app/src/main/java/com/example/agroproject/MapActivity.java
@@ -1,19 +1,61 @@
 package com.example.agroproject;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.agroproject.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+import java.io.IOException;
+import java.util.List;
+
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // Location permission request code
+    private static final int LOCATION_PERMISSION_CODE = 1;
+
+    // Google's API for location service
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    // Location callback
+    private LocationCallback locationCallback;
+
+    // Location request
+    private LocationRequest locationRequest;
+    //-------------------------------------------
+
+    //Google Map
     private GoogleMap mMap;
+
+    //Binding
     private ActivityMapsBinding binding;
+
+    // current view
+    private View currentView;
+
+    // Device coordinates
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +64,70 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Current view
+        currentView = findViewById(android.R.id.content);
+
+        // Initialize FusedLocationProviderClient object
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Initialize a location request
+        locationRequest = new LocationRequest();
+
+        //PRIORITY_HIGH_ACCURACY using from the gps
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Location callBack method
+        locationCallBackExecute();
+
+        // Permission check service
+        checkPermissions();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+
+    public void checkPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            //permission question
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+
+        } else {
+            //start location service
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        }
+    }
+
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == LOCATION_PERMISSION_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+
+            }else{
+                Snackbar.make(currentView, "Permission is not accepted", Snackbar.LENGTH_LONG).show();
+            }
+        }
     }
 
     /**
@@ -39,18 +141,61 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // Initialize map
         mMap = googleMap;
 
-        //set satellite map
+        // Setup satellite map
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-        // Add a marker in Sydney and move the camera
-        LatLng location = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(location).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 
 
+    /*
+      This method handle the requestLocationUpdates.
+      Used for receiving notifications from the FusedLocationProviderApi
+      when the device location has changed or can no longer be determined.
+      */
+    private void locationCallBackExecute(){
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
 
+                    //get the latitude
+                    latitude = location.getLatitude();
 
+                    //get the longitude
+                    longitude = location.getLongitude();
+
+                    //instantiate the class LatLng
+                    LatLng currentLocation = new LatLng(latitude,longitude);
+
+                    //instantiate the class Geocoder
+                    Geocoder geocoder = new Geocoder(MapActivity.this.getApplicationContext());
+
+                    mMap.clear();
+
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+
+                        //Add a marker in current location and move the camera
+                        mMap.addMarker(new MarkerOptions().position(currentLocation).title(addressList.get(0).getAddressLine(0)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 19.2f));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //stop requestLocationUpdates method from FusedLocationProviderClient service
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
 }

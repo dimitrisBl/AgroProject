c380f22ebc123be626f784f5c2b8799c4950b1e3
Dimitris <dimitrisbl@hotmail.com>
2020-12-10 19:04:05 +0200

    google satellite map implementation
    initialize satellite map

---
+++ app/src/main/java/com/example/agroproject/ActivityMain.java
@@ -0,0 +1,38 @@
+package com.example.agroproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityMain extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.map_btn);
+        button.setOnClickListener(onButtonClickListener);
+
+    }
+
+
+
+    private View.OnClickListener onButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button currentButton = (Button) view;
+
+            if(currentButton.getText().equals("Open Map")){
+                Intent intent = new Intent(ActivityMain.this, MapActivity.class);
+                startActivity(intent);
+            }
+
+        }
+    };
+}

ba4d275e21d9513d50f6f69da4f645ed496357d2
Dimitris <dimitrisbl@hotmail.com>
2021-02-03 04:00:06 +0200

    comments

--- app/src/main/java/com/example/agroproject/model/PolygonModel.java
+++ app/src/main/java/com/example/agroproject/model/PolygonModel.java
@@ -17,25 +17,29 @@ public class PolygonModel {
     // Class TAG
     private final String TAG = "PolygonModel";
 
+    // Name for the Shared preferences file
     private final String PREFS_NAME ="PolygonOptionsSave";
 
-    private final String POLYGON_OPTIONS_LIST = "PolygonOptionsList";
-
+    // SharedPreferences
     private SharedPreferences polygonStatePrefs;
 
+    // Name for the polygonOptionsList in shared preferences file
+    private final String POLYGON_OPTIONS_LIST = "PolygonOptionsList";
+
+    // List with PolygonOptions objects
     private List<PolygonOptions> polygonOptionsList = new ArrayList<>();
 
     /**
-     *
-     * @param context
+     * This method initialize the polygonStatePrefs object.
+     * @param context takes the current context application.
      */
     public PolygonModel(Context context){
         polygonStatePrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
     }
 
     /**
-     *
-     * @param polygonOptions
+     * This method adds polygonOptions in the polygonOptionsList.
+     * @param polygonOptions represent a polygon area.
      */
     public void addPolygonOptions(PolygonOptions polygonOptions){
         Log.d(TAG,"PolygonOptions were added in the polygonOptionsList.");
@@ -43,11 +47,10 @@ public class PolygonModel {
     }
 
     /**
-     * TODO METHOD DESCRIPTION
-     *
+     * This method save polygonOptionsList in shared preferences file.
      */
     public void savePolygonOptions(){
-        Log.d(TAG,"Saving PolygonOptions in shared preferences.");
+        Log.d(TAG,"PolygonOptions save executed");
         // Instantiate the gson object.
         Gson gson = new Gson();
         //Convert java object as a json string.
@@ -60,24 +63,18 @@ public class PolygonModel {
     }
 
     /**
-     * TODO METHOD DESCRIPTION
-     * @return
+     * This method receives PolygonOptions from the shared preferences saved file.
+     * @return the List it contains polygonOptions from the save file.
      */
     public List<PolygonOptions> getSavedPolygonOptions(){
         String serializedObject = polygonStatePrefs.getString(POLYGON_OPTIONS_LIST, null);
         if (serializedObject != null) {
             // Instantiate the gson object.
             Gson gson = new Gson();
+
             Type type = new TypeToken<List<PolygonOptions>>(){}.getType();
             polygonOptionsList = gson.fromJson(serializedObject, type);
         }
         return polygonOptionsList;
-    }
-
-
-    public void clearSharedPrefsData(){
-        SharedPreferences.Editor editor = polygonStatePrefs.edit();
-        editor.clear();
-        editor.commit();
     }
 }

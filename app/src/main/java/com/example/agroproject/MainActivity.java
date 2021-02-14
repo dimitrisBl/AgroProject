18513396d62ee3f6b274bc9111371ca0d4099f2d
Dimitris <dimitrisbl@hotmail.com>
2021-02-14 03:29:28 +0200

    changes

--- app/src/main/java/com/example/agroproject/MainActivity.java
+++ app/src/main/java/com/example/agroproject/MainActivity.java
@@ -7,25 +7,22 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.material.snackbar.Snackbar;
+
+import com.example.agroproject.services.LocationService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
     /** Permission request code */
     private static final int LOCATION_PERMISSION_CODE = 1;
 
-
+    /** Device coordinates */
     private double latitude;
-
     private double longitude;
+
 
     // current view
     private View currentView;
@@ -66,52 +63,119 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Permission check service
+        checkPermissions();
+    }
 
-
-        if(Build.VERSION.SDK_INT >= 16){
-
-           if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+    /**
+     *  This method check if location permission granted.
+     *  If the permission has been granted calls the startLocationService method to start a location service.
+     *  If the permission has not been granted displays a request for the missing permissions and asks the permission.
+     */
+    private void checkPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            // public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            // to handle the case where the user grants the permission.
 
             //permission question
             ActivityCompat.requestPermissions(this, new String[]
                     {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
-
-           }else{
-               //Start the location service
-               startLocationService();
-           }
-
-        }else{
-            //Start the location service
+        } else {
+            // Start the location service
             startLocationService();
         }
     }
 
+    /**
+     * TODO DESCRIPTION
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == LOCATION_PERMISSION_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Location permission granted!");
+                // Start the location service
+                startLocationService();
+            }else{
+                // Location permission not granted
+                Toast.makeText(MainActivity.this,
+                        "Accept this permission for use map and other services",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    /**
+     * This method ask for gps enable if the gps of the device is off, else
+     * if the gps is enable  he don't any something.
+     * TODO MORE DESCRIPTION
+     * @return boolean, true if gps status is enable or false if gps status is disable.
+     */
+    private boolean isGpsEnable(){
+        // Initialize a LocationManager object
+        LocationManager locationManager =
+                (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Get GPS provider status
+        boolean providerEnable = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(providerEnable){
+            // GPS is enable
+            return true;
+        }else{
+            // GPS is not enable
+            AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("GPS permission")
+                    .setMessage("GPS is required for use map and other services. Please enable GPS")
+                    .setPositiveButton("Yes", ((dialogInterface, i) -> {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }))
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this,
+                                    "GPS is required for use map and other services. Please enable GPS",Toast.LENGTH_LONG).show();
+                        }
+                    })
+            .show();
+        }
+        return  false;
+    }
+
+
+
+    /**
+     *  This method starts an intent service
+     *  in LocationService class.
+     */
     public void startLocationService(){
         Intent locationServiceIntent = new Intent(this, LocationService.class);
         startService(locationServiceIntent);
     }
 
-
-
-
-    // Our handler for received Intents. This will be called whenever an Intent
-    // with an action named "custom-event-name" is broadcasted.
-    private BroadcastReceiver mMessageReceiver  = new BroadcastReceiver() {
+    /**
+     *  Our handler for received Intents. This will be called whenever an Intent
+     *  with an action named "GPSLocationUpdates".
+     *  TODO MORE DESCRIPTION
+     */
+    private BroadcastReceiver locationReceiver  = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             Toast.makeText(MainActivity.this,
                     "Receive coordinates in main activity: "+latitude,Toast.LENGTH_SHORT).show();
 
-            // Get extra data included in the Intent
             // Get extra data included in the Intent
              latitude = intent.getDoubleExtra("latitude",0.0);
              longitude = intent.getDoubleExtra("longitude",0.0);
@@ -121,47 +185,22 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-//    @Override
-//    protected void onNewIntent(Intent intent) {
-//        super.onNewIntent(intent);
-//        Toast.makeText(this," im over here", Toast.LENGTH_LONG).show();
-//    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if(requestCode == LOCATION_PERMISSION_CODE){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-
-                startLocationService();
-            }else{
-                Snackbar.make(currentView, "Permission is not accepted", Snackbar.LENGTH_LONG).show();
-            }
-        }
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
         // Receive messages about current location.
         // We are registering an observer (GPSLocationUpdates) to receive Intents with actions named "LocationService".
         LocalBroadcastManager.getInstance(this).registerReceiver(
-                mMessageReceiver, new IntentFilter("GPSLocationUpdates"));
+                locationReceiver, new IntentFilter("GPSLocationUpdates"));
+
+        // GPS status
+        isGpsEnable();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         // Unregister since the activity is about to be closed.
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(locationReceiver);
     }
-
-
-    //    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        // Unregister since the activity is about to be closed.
-//        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
-//    }
 }
--- app/src/main/java/com/example/agroproject/services/LocationService.java
+++ app/src/main/java/com/example/agroproject/services/LocationService.java
@@ -1,4 +1,4 @@
-package com.example.agroproject;
+package com.example.agroproject.services;
 
 import android.annotation.SuppressLint;
 import android.app.Service;
@@ -19,11 +19,12 @@ import com.google.android.gms.location.LocationServices;
 
 /**
  *  TODO CLASS DESCRIPTION
+ *
  *  */
 public class LocationService extends Service {
 
     /** Class TAG */
-    private final String TAG = "LocationService";
+    private final String TAG = "LocationTrackingService";
 
     /** Google's API for location service */
     private FusedLocationProviderClient fusedLocationProviderClient;
@@ -53,7 +54,7 @@ public class LocationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("LocationService","service started");
+        Log.d(TAG,"service started");
         // Start request for location
         requestLocation();
         return super.onStartCommand(intent, flags, startId);
@@ -69,8 +70,8 @@ public class LocationService extends Service {
         locationRequest = new LocationRequest();
 
         // For high accuracy location
-        locationRequest.setInterval(5000); //5 second
-        locationRequest.setFastestInterval(2000);
+        locationRequest.setInterval(1000); //5 second
+        locationRequest.setFastestInterval(1000);
         //locationRequest.setSmallestDisplacement(1); //1 metro
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
@@ -82,17 +83,17 @@ public class LocationService extends Service {
      * This method send an intent with an action "GPSLocationUpdates".
      * The intent sent is received by the Activity.
      *
-     * @param locationResult receives the current device location.
+     * @param location receives the current device location.
      *
      */
-    private  void sendMessageToActivity(LocationResult locationResult) {
+    private  void sendMessageToActivity(Location location) {
         Log.d(TAG, "sending message on activity");
         // Instantiate an intent
         Intent intent = new Intent("GPSLocationUpdates");
 
         // Include the device coordinates in intent
-        intent.putExtra("latitude", locationResult.getLastLocation().getLatitude());
-        intent.putExtra("longitude", locationResult.getLastLocation().getLongitude());
+        intent.putExtra("latitude", location.getLatitude());
+        intent.putExtra("longitude", location.getLongitude());
 
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
@@ -110,7 +111,7 @@ public class LocationService extends Service {
             public void onLocationResult(LocationResult locationResult) {
                 for (Location location : locationResult.getLocations()) {
                     // Send coordinates in MainActivity
-                    sendMessageToActivity(locationResult);
+                    sendMessageToActivity(location);
                 }
 
             }
